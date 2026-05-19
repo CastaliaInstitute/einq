2c194ef8478e052b791d8fcf68cbd14e3b0437ed
dcmcshan <dan@syzygyx.com>
2026-05-18 21:16:33 -0600

    Fix BLE snapshot struct sizes and fail CI when firmware build fails.

--- firmware/einq-ble/EinqDisplaySnapshot.h
+++ firmware/einq-ble/EinqDisplaySnapshot.h
@@ -24,9 +24,9 @@ struct EinqDisplaySnapshot {
 /** Parsed display command from BLE write (show message or return to clock). */
 struct EinqDisplayCommand {
   bool valid = false;
-  char mode[kModeLen] = "clock";
-  char title[kTitleLen] = "Einq";
-  char line1[kLineLen] = {};
-  char line2[kLineLen] = {};
-  char line3[kLineLen] = {};
+  char mode[EinqDisplaySnapshot::kModeLen] = "clock";
+  char title[EinqDisplaySnapshot::kTitleLen] = "Einq";
+  char line1[EinqDisplaySnapshot::kLineLen] = {};
+  char line2[EinqDisplaySnapshot::kLineLen] = {};
+  char line3[EinqDisplaySnapshot::kLineLen] = {};
 };
